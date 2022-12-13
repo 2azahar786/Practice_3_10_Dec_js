0a1382e39fc9df197806e32ae481c809238f4895
2azahar786 <2azahar786@gmail.com>
2022-12-13 18:52:13 +0530

    initialcommit

---
+++ index_script.js
@@ -0,0 +1,153 @@
+// History
+// Javascript came in 1995 name as "mocha"
+// Its changed in 1996 name as "live script"
+// against its changes in 1999 name as javascript and this as till Date
+
+// why need java Script
+// that time there were two browser first is "internet explorer" and 2nd is "netskip"
+// which is supported only html and css File
+// the browser get some shorts of functinality so for the solutions of that they created scriting langues for that
+// programmer were creating own script so it was very dificullt inderstand for other programer.
+// Echmascript organisation came into the pictures who set certain rules for the scripting langues which will be 
+// applied to all scripting langues. it set the rules and becomes midified and in 2015 gets satndard rules name as ES6
+
+// 6 pillors of any programming langues
+// 1. Values 2. operation 3. variables 
+// 4. conditions 5. loops6. functions
+
+
+// Values
+// 9;
+// 3.5;
+// these are the number values
+
+// Azhar;
+// Ansari;
+//  These are the string values
+
+// true; (1)
+// False; (0)
+
+// these are the Boolean values(Itwas implemented by the famous sceintis name as george boole)
+
+// null;
+// undefined;
+// These are te empty values
+
+// These all are the premitive values (Premitive has a single value)
+
+// Array And Object
+
+// [1, 'azhar', 'ansari', 85, true, 65, 8] (in squqre brackets are aaray and it should be Number, string, boolean)
+//  0,  1,         2,      3,   4,   5,  6 - index value tells the place of that aaray value
+
+
+// {
+//  firstname:Azhar
+//  lastname:Ansari
+
+// }
+ 
+// here are the key and values, key will be your choice.these are the object and it should be always in curly brackets
+
+// this is the non premitive data type= has more than one value
+
+
+// Variables
+
+// var (this will be taught later)
+// let
+// const
+
+// let x=20; (this will be cahned any time)
+// const bankaccountno='45236178933155' (this cant't be changed never)
+
+
+// let firstname='azhar';
+// let lastname='ansari';
+// const accountno=45789611233;
+
+// console.log( 'My first name is'+firstname);
+// console.log('My last name is',lastname);
+// console.log(accountno,firstname,lastname);
+
+// Template Literal
+//  console.log(`My current first name is ${firstname} ${lastname} ${accountno}`);
+
+// Operator
+
+// Assignment operator
+
+// let x=10;
+// let y=12;
+
+
+
+// Arithmatic operator
+
+// +, -, *, /, (%=> remainder)
+// let z=x+y;
+
+// console.log(z);
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+// comparision operator
+
+// let abc=20;
+// let xyz=25;
+
+// console.log(abc>xyz);
+// console.log(abc<xyz);
+// console.log(abc<=xyz);
+// console.log(abc>=xyz);
+
+// == or ===
+
+// let az=10;
+// let an='10';
+
+// console.log(az==an); (this will compare only value)
+// console.log(az===an); (this will compare data tyes)
+
+// Interview question
+
+// let y;
+// y=10;
+
+// console.log(y);
+
+// const account=456321;
+// // account=56324;
+// console.log(account);
+
+
+// conditions
+
+// let age=31;
+
+// if ( age==18)
+// {
+// console.log('you are allowed to the park');
+
+// }
+
+// else if(age<=21){
+//     console.log('You are welcome');
+// }
+
+// else if(age>=30){
+//     console.log('you are most welcome');
+// }
+// else{
+    // console.log('you are not allowed in the park')
+// }
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Full Stack Javascript Bootcamp 2.0
+
+## Javascript started with basic
+
+**Histry of the javascript**
+
